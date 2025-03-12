85ed23c7997f873a9246b375d1b1895438b11da8
hmelihf <haldunmelih@gmail.com>
2025-03-12 16:59:26 +0300

    feat(str-compose): create compose lazy pagination

--- android/kotlin/StorylyJetpackCompose/app/src/main/java/com/example/storylyjetpackcompose/MainActivity.kt
+++ android/kotlin/StorylyJetpackCompose/app/src/main/java/com/example/storylyjetpackcompose/MainActivity.kt
@@ -1,25 +1,37 @@
 package com.example.storylyjetpackcompose
 
 import android.os.Bundle
-import android.util.Log
+import android.view.ViewGroup
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import com.appsamurai.storyly.*
-import com.appsamurai.storyly.analytics.StorylyEvent
-import com.appsamurai.storyly.config.StorylyConfig
-import com.appsamurai.storyly.config.styling.group.StorylyStoryGroupStyling
-import com.appsamurai.storyly.config.styling.story.StorylyStoryStyling
+import androidx.paging.PagingData
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.appsamurai.storyly.StorylyInit
+import com.appsamurai.storyly.StorylyView
 import com.example.storylyjetpackcompose.ui.theme.StorylyJetpackComposeTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
+    private val dummyItems = createDummyFlow()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,89 +41,86 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    StorylyView("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40")
+                    val storylyView = createStorylyView(StorylyInitItem("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"))
+                    val pagingList = dummyItems.collectAsLazyPagingItems()
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                    ) {
+                        items(pagingList.itemCount, key = { itemId -> itemId }) { itemId ->
+                            val pagingItem = pagingList[itemId]
+                            when (pagingItem) {
+                                is StorylyInitItem -> storylyView?.let {
+                                    println("TEST: lazy: compose")
+                                    StorylyView(storylyView)
+                                }
+                                is DummyItem -> {
+                                    Box(Modifier.fillMaxWidth().height(120.dp).background(pagingItem.color))
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
+
+    fun createDummyFlow(): MutableStateFlow<PagingData<PagingItem>> {
+        val fakeData: List<PagingItem> = List(10) {
+            if (it == 0) {
+                StorylyInitItem("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40")
+            } else {
+                DummyItem(Color(
+                    red = Random.nextFloat(),
+                    green = Random.nextFloat(),
+                    blue = Random.nextFloat(),
+                    alpha = 1f
+                ))
+            }
+        }
+        val pagingData = PagingData.from(fakeData)
+        return MutableStateFlow(pagingData)
+    }
 }
 
 @Composable
-fun StorylyView(token: String) {
-    val token = remember { mutableStateOf(token) }
+fun createStorylyView(
+    item: StorylyInitItem?
+): StorylyView? {
+    item ?: return null
+    println("TEST: createStorylyView: inside")
+    val context = LocalContext.current
+    val storylyView = remember(item) {
+        StorylyView(context).apply {
+            this.storylyInit = StorylyInit(item.token)
+        }
+    }
+    return storylyView
+}
 
-    // Adds view to Compose
-    AndroidView(
-        modifier = Modifier.fillMaxSize(), // Occupy the max size in the Compose UI tree
-        factory = { context ->
-            // Create StorylyView
-            StorylyView(context)
+@Composable
+fun StorylyView(storylyView: StorylyView?) {
+    storylyView ?: return
+
+    AndroidView(modifier = Modifier
+        .fillMaxWidth(),
+        factory = { _ ->
+            // Ensure the storylyView is removed from its parent if it has one
+            (storylyView.parent as? ViewGroup)?.removeView(storylyView)
+            storylyView
         },
         update = { view ->
-            view.storylyInit = StorylyInit(
-                token.value,
-                StorylyConfig.Builder()
-                    .build()
-            )
-            view.storylyListener = object : StorylyListener {
-                override fun storylyLoaded(
-                    storylyView: StorylyView,
-                    storyGroupList: List<StoryGroup>,
-                    dataSource: StorylyDataSource
-                ) {
-                    super.storylyLoaded(storylyView, storyGroupList, dataSource)
-                    Log.d("[Storyly]", "storylyLoaded:${storyGroupList.size}")
-                }
-
-                override fun storylyLoadFailed(storylyView: StorylyView, errorMessage: String) {
-                    super.storylyLoadFailed(storylyView, errorMessage)
-                    Log.d("[Storyly]", "storylyLoadFailed:errorMessage:$errorMessage")
-                }
-
-                override fun storylyActionClicked(storylyView: StorylyView, story: Story) {
-                    // Navigate
-                    storylyView.pauseStory()
-                }
-
-                override fun storylyStoryShown(storylyView: StorylyView) {
-                    super.storylyStoryShown(storylyView)
-                    Log.d("[Storyly]", "storylyStoryShown")
-                }
-
-                override fun storylyStoryDismissed(storylyView: StorylyView) {
-                    super.storylyStoryDismissed(storylyView)
-                    Log.d("[Storyly]", "storylyStoryDismissed")
-                }
-
-                override fun storylyStoryShowFailed(
-                    storylyView: StorylyView,
-                    errorMessage: String
-                ) {
-                    super.storylyStoryShowFailed(storylyView, errorMessage)
-                    Log.d("[Storyly]", "storylyStoryShowFailed:$errorMessage")
-                }
-
-                override fun storylyUserInteracted(
-                    storylyView: StorylyView,
-                    storyGroup: StoryGroup,
-                    story: Story,
-                    storyComponent: StoryComponent
-                ) {
-                    super.storylyUserInteracted(storylyView, storyGroup, story, storyComponent)
-                    Log.d("[Storyly]", "storylyStoryLayerInteracted")
-                }
-
-                override fun storylyEvent(
-                    storylyView: StorylyView,
-                    event: StorylyEvent,
-                    storyGroup: StoryGroup?,
-                    story: Story?,
-                    storyComponent: StoryComponent?
-                ) {
-                    super.storylyEvent(storylyView, event, storyGroup, story, storyComponent)
-                    Log.d("[Storyly]", "storylyEvent:${event.name}")
-                }
-            }
         }
     )
 }
+
+
+interface PagingItem
+
+data class DummyItem(
+    val color: Color,
+) : PagingItem
+
+data class StorylyInitItem(
+    val token: String,
+) : PagingItem
